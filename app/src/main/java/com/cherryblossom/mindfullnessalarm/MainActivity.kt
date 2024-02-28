f3d77f41dd10172b92eb0ad688d7d286cfcc981e
pericles <pericleskb@gmail.com>
2024-02-28 22:54:59 +0200

    create TimeTextField

--- app/src/main/java/com/cherryblossom/mindfullnessalarm/MainActivity.kt
+++ app/src/main/java/com/cherryblossom/mindfullnessalarm/MainActivity.kt
@@ -1,36 +1,46 @@
 package com.cherryblossom.mindfullnessalarm
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalFontFamilyResolver
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.createFontFamilyResolver
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cherryblossom.mindfullnessalarm.composables.dialogs.PickTimeDialog
+import com.cherryblossom.mindfullnessalarm.composables.text.TimeTextField
 import com.cherryblossom.mindfullnessalarm.ui.theme.MindfullnessAlarmTheme
-import kotlinx.coroutines.CoroutineExceptionHandler
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,35 +65,31 @@ fun ScreenHost() {
     Column(
         horizontalAlignment = Alignment.End,
         verticalArrangement = Arrangement.Top,
-        modifier = Modifier.padding(16.dp).fillMaxWidth()
+        modifier = Modifier.padding(16.dp).fillMaxSize()
     ) {
-        var isDialogVisible by remember { mutableStateOf(false) }
-        CompositionLocalProvider(
-            LocalFontFamilyResolver provides createFontFamilyResolver(LocalContext.current, handler)
-        ) {
-            Text(text = "press me damn it",
-                modifier = Modifier.clickable {
-                isDialogVisible = !isDialogVisible
-            })
-        }
-        if (isDialogVisible) {
+        Spacer(modifier = Modifier.fillMaxHeight(0.1f))
+        var startTimeDialogVisible by remember { mutableStateOf(false) }
+        var endTimeDialogVisible by remember { mutableStateOf(false) }
+        TimeTextField(
+            text = "09:00",
+            label = stringResource(R.string.set_earliest_time),
+            onClick = { startTimeDialogVisible = !startTimeDialogVisible },
+            modifier = Modifier.clip(RoundedCornerShape(16.dp))
+                .fillMaxWidth()
+        )
+        if (startTimeDialogVisible) {
             PickTimeDialog(stringResource(R.string.set_earliest_time),
-                onDismissRequest = { isDialogVisible = false },
+                onDismissRequest = { startTimeDialogVisible = false },
                 onAcceptRequest = fun (hour: Int, minute: Int) {
                     println("$hour:$minute")
-                    isDialogVisible = false
+                    startTimeDialogVisible = false
                 }
             )
         }
     }
 }
 
-val handler = CoroutineExceptionHandler { _, throwable ->
-    // process the Throwable
-    Log.e("@@@  ", "There has been an issue: ", throwable)
-}
-
-@Preview(showSystemUi = true, name = "PIXEL 4", device = Devices.PIXEL_4)
+//@Preview(showSystemUi = true, name = "PIXEL 4", device = Devices.PIXEL_4)
 //@Preview(showSystemUi = true, name = "NEXUS 5", device = Devices.NEXUS_5)
 //@Preview(name = "NEXUS_7", device = Devices.NEXUS_7)
 @Composable
---
+++ app/src/main/java/com/cherryblossom/mindfullnessalarm/composables/text/TimeTextField.kt
@@ -0,0 +1,2 @@
+package com.cherryblossom.mindfullnessalarm.composables.text
+
