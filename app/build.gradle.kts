65a41e86392c2de270d0df42eeb4eca94550eceb
pericles <pericleskb@gmail.com>
2024-03-22 16:52:49 +0200

    update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,23 +55,24 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
     implementation("androidx.compose.material3:material3:1.2.1")
-    implementation ("androidx.compose.ui:ui-text-google-fonts:1.6.3")
-    implementation ("androidx.compose.compiler:compiler:1.5.10")
+    implementation ("androidx.compose.ui:ui-text-google-fonts:1.6.4")
+    implementation ("androidx.compose.compiler:compiler:1.5.11")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     implementation ("com.chargemap.compose:numberpicker:1.0.3")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
